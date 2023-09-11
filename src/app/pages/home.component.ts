c7146ec5e687b148618a05c1ee19213791d9704f
warrengalyen <wgalyen@hotmail.com>
2023-09-11 09:38:53 -0700

    Add routed dialog and settings

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -3,24 +3,21 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
+    <a [routerLink]="[{ outlets: { dialog: ['settings'] } }]">
       home works!
-    </p>
+    </a>
   `,
   styles: [
     `
       :host {
         display: block;
       }
-    `
+    `,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor() {}
+  ngOnInit(): void {}
 
 }
