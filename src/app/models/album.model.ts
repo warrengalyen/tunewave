debaa71d7b3611905ba18ef4fef2742fe35d27c8
warrengalyen <wgalyen@hotmail.com>
2023-09-15 08:38:26 -0700

    extract albums/artists and save to db

--- src/app/models/album.model.ts
+++ src/app/models/album.model.ts
@@ -1,7 +1,11 @@
+import { Except } from '@app/utils/types.util';
+
 export interface Album {
   id: string;
-  name?: string;
-  artist?: string;
+  name: string;
   artistId: string;
   year?: number;
+  pictureKey?: IDBValidKey;
 }
+
+export type AlbumWithCover = Except<Album, 'pictureKey'> & { cover?: string };
