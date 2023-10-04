316810806d876fd8989f35b919672e9b95ff1a78
warrengalyen <wgalyen@hotmail.com>
2023-10-04 12:31:35 -0700

    add cypress and fix index name

---
+++ cypress/integration/app/app.spec.ts
@@ -0,0 +1,30 @@
+describe('TuneWave', () => {
+  before(() => cy.deleteDatabase('tunewave'));
+
+  it('should redirect to /welcome', () => {
+    cy.visit('/');
+    cy.url().should('include', '/welcome');
+    cy.contains('TuneWave');
+    expect(localStorage.getItem('scanned')).to.be.null;
+  });
+
+  it('should display library when scanned=1', () => {
+    localStorage.setItem('scanned', '1');
+    cy.visit('/');
+    cy.url().should('include', '/library');
+    cy.contains('TuneWave');
+    expect(localStorage.getItem('scanned')).to.eq('1');
+    cy.clearLocalStorage().should((ls) => {
+      expect(ls.getItem('scanned')).to.be.null;
+    });
+  });
+
+  it('should clear database and redirect to /welcome', () => {
+    localStorage.setItem('scanned', '1');
+    cy.visit('/');
+    cy.url().should('include', '/library');
+    cy.get('app-menu').click();
+    cy.contains('Clear database').click();
+    cy.url().should('include', '/welcome');
+  });
+});
