790197749780a801d01310114f934457f16f701a
warrengalyen <wgalyen@hotmail.com>
2023-10-17 09:17:30 -0700

    refactor: helper facade implementation

--- src/app/core/services/component-helper.service.ts
+++ src/app/core/services/component-helper.service.ts
@@ -1,13 +1,7 @@
 import { Injectable } from '@angular/core';
-import { first, tap } from 'rxjs/operators';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { combineLatest } from 'rxjs';
-import { Song } from '@app/database/songs/song.model';
-import { PlayerFacade } from '@app/player/store/player.facade';
 
 @Injectable()
 export class ComponentHelperService {
-  constructor(private player: PlayerFacade, private snack: MatSnackBar) {}
 
   // toggleLikedSong(song: Song): Observable<Song> {
   //   return this.library
@@ -21,7 +15,6 @@ export class ComponentHelperService {
   //       )
   //     );
   // }
-
   // toggleLikedAlbum(album: Album): void {
   //   this.library.toggleLikedAlbum(album);
   //   // return (
@@ -135,65 +128,65 @@ export class ComponentHelperService {
   //   // );
   // }
 
-  playNext(song: Song): void {
-    this.player.addToPlaylist([song.entryPath], true);
-    this.player.show();
-    this.openSnack('Song will play next');
-  }
+  // playNext(song: Song): void {
+  //   this.player.addToQueue([song.entryPath], true);
+  //   this.player.show();
+  //   this.openSnack('Song will play next');
+  // }
+  //
+  // addToQueue(song: Song): void {
+  //   this.player.addToQueue([song.entryPath]);
+  //   this.player.show();
+  //   this.openSnack('Song added to queue');
+  // }
 
-  addToQueue(song: Song): void {
-    this.player.addToPlaylist([song.entryPath]);
-    this.player.show();
-    this.openSnack('Song added to queue');
-  }
+  // removeFromQueue(song: Song): void {
+  //   combineLatest([this.player.getPlaylist$(), this.player.getCurrentIndex$()])
+  //     .pipe(
+  //       first(),
+  //       tap(([playlist, index]) => {
+  //         const newPlaylist = [...playlist];
+  //         newPlaylist.splice(playlist.indexOf(song.entryPath), 1);
+  //         this.player.setQueue(
+  //           newPlaylist,
+  //           Math.min(index, newPlaylist.length - 1)
+  //         );
+  //       })
+  //     )
+  //     .subscribe();
+  // }
 
-  removeFromQueue(song: Song): void {
-    combineLatest([this.player.getPlaylist$(), this.player.getCurrentIndex$()])
-      .pipe(
-        first(),
-        tap(([playlist, index]) => {
-          const newPlaylist = [...playlist];
-          newPlaylist.splice(playlist.indexOf(song.entryPath), 1);
-          this.player.setPlaylist(
-            newPlaylist,
-            Math.min(index, newPlaylist.length - 1)
-          );
-        })
-      )
-      .subscribe();
-  }
+  // openSnack(message: string): void {
+  //   this.player
+  //     .isShown$()
+  //     .pipe(
+  //       first(),
+  //       tap((shown) =>
+  //         this.snack.open(message, undefined, {
+  //           panelClass: shown ? 'snack-top' : 'snack',
+  //         })
+  //       )
+  //     )
+  //     .subscribe();
+  // }
 
-  openSnack(message: string): void {
-    this.player
-      .isShown$()
-      .pipe(
-        first(),
-        tap((shown) =>
-          this.snack.open(message, undefined, {
-            panelClass: shown ? 'snack-top' : 'snack',
-          })
-        )
-      )
-      .subscribe();
-  }
+  // shufflePlaySongs(songs: Song[]): void {
+  //   this.player.setPlaying();
+  //   this.player.setQueue(songs.map((s) => s.entryPath));
+  //   this.player.shuffle();
+  //   this.player.show();
+  // }
 
-  shufflePlaySongs(songs: Song[]): void {
-    this.player.setPlaying();
-    this.player.setPlaylist(songs.map((s) => s.entryPath));
-    this.player.shuffle();
-    this.player.show();
-  }
-
-  addSongsToQueue(songs: Song[], next = false): void {
-    this.player.addToPlaylist(
-      songs.map((s) => s.entryPath),
-      next
-    );
-    this.player.show();
-    if (next) {
-      this.openSnack('Songs will play next');
-    } else {
-      this.openSnack('Songs added to queue');
-    }
-  }
+  // addSongsToQueue(songs: Song[], next = false): void {
+  //   this.player.addToQueue(
+  //     songs.map((s) => s.entryPath),
+  //     next
+  //   );
+  //   this.player.show();
+  //   if (next) {
+  //     this.openSnack('Songs will play next');
+  //   } else {
+  //     this.openSnack('Songs added to queue');
+  //   }
+  // }
 }
