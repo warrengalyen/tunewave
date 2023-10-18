df9d56494b193526ed5fcab49fb6823f4ffd88cf
warrengalyen <wgalyen@hotmail.com>
2023-10-18 07:47:05 -0700

    feat: playlists persistence and deletion

--- src/app/database/playlists/playlist.effects.ts
+++ src/app/database/playlists/playlist.effects.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, concatMap, map } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
 import {
-  loadPlaylists,
-  loadPlaylistsFailure,
-  loadPlaylistsSuccess,
+    addPlaylist,
+    deletePlaylist,
+    loadPlaylists,
+    loadPlaylistsFailure,
+    loadPlaylistsSuccess,
+    updatePlaylist,
 } from './playlist.actions';
 import { Playlist } from '@app/database/playlists/playlist.model';
 import { DatabaseService } from '@app/database/database.service';
@@ -13,43 +16,54 @@ import { DatabaseService } from '@app/database/database.service';
 // noinspection JSUnusedGlobalSymbols
 @Injectable()
 export class PlaylistEffects {
-  loadPlaylists$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(loadPlaylists),
-      concatMap(() =>
-        this.database.getAll$<Playlist>('playlists').pipe(
-          map((data) => loadPlaylistsSuccess({ data })),
-          catchError((error) => of(loadPlaylistsFailure({ error })))
+    loadPlaylists$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(loadPlaylists),
+            concatMap(() =>
+                this.database.getAll$<Playlist>('playlists').pipe(
+                    map((data) => loadPlaylistsSuccess({ data })),
+                    catchError((error) => of(loadPlaylistsFailure({ error })))
+                )
+            )
         )
-      )
-    )
-  );
+    );
 
-  // addPlaylist$ = createEffect(
-  //   () =>
-  //     this.actions$.pipe(
-  //       ofType(addPlaylist),
-  //       concatMap(({ playlist }) =>
-  //         this.database
-  //           .add$<Playlist>('playlists', playlist)
-  //           .pipe(catchError(() => EMPTY))
-  //       )
-  //     ),
-  //   { dispatch: false }
-  // );
-  //
-  // updatePlaylist$ = createEffect(
-  //   () =>
-  //     this.actions$.pipe(
-  //       ofType(updatePlaylist),
-  //       concatMap(({ update }) =>
-  //         this.database
-  //           .update$<Playlist>('playlists', update.changes, update.key)
-  //           .pipe(catchError(() => EMPTY))
-  //       )
-  //     ),
-  //   { dispatch: false }
-  // );
+    addPlaylist$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(addPlaylist),
+                concatMap(({ playlist }) =>
+                    this.database
+                        .add$<Playlist>('playlists', playlist)
+                        .pipe(catchError(() => EMPTY))
+                )
+            ),
+        { dispatch: false }
+    );
 
-  constructor(private actions$: Actions, private database: DatabaseService) {}
+    updatePlaylist$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(updatePlaylist),
+                concatMap(({ update }) =>
+                    this.database
+                        .update$<Playlist>('playlists', update.changes, update.key)
+                        .pipe(catchError(() => EMPTY))
+                )
+            ),
+        { dispatch: false }
+    );
+
+    deletePlaylist$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(deletePlaylist),
+                concatMap(({ id }) =>
+                    this.database.delete$('playlists', id).pipe(catchError(() => EMPTY))
+                )
+            ),
+        { dispatch: false }
+    );
+
+    constructor(private actions$: Actions, private database: DatabaseService) {}
 }
